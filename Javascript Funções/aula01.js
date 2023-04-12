a3f937fe05dd9cf5a6639902d7fcf218295278ba
Bandeira Magalhães <bandeiramagalhaesoficial@gmail.com>
2023-04-11 23:29:06 -0300

    concluido o bloco 12 do curso

--- Javascript Funções/aula01.js
+++ Javascript Funções/aula01.js
@@ -49,8 +49,89 @@ function conta(operator, acumulator, ...rest) {
   }
 }
 
-console.log(conta('+', 0, 20, 30, 40, 50));
+// console.log(conta('+', 0, 20, 30, 40, 50));
 
 // arrayFunction(['banana', 'macaco', 'colega']);
 
 // objectionFunction({ nome: 'Luiz', sobrenome: 'Otávio', idade: 25, peso: 80 });
+
+// RETORNO DAS FUNÇÕES
+
+function sum(a, b) {
+  // Retorna algo
+  // Encerra a função
+  return a + b;
+}
+
+function speakPhrase(init) {
+  function speakFinal(final) {
+    return init + ' ' + final;
+  }
+  return speakFinal;
+}
+
+const helloWorld = speakPhrase('Hello');
+// console.log(helloWorld('World!'));
+
+// ESCOPO LÉXICO
+const name = 'Bandeira';
+
+function speakName() {
+  console.log(name);
+}
+
+function useSpeakName() {
+  const name = 'Neto';
+  speakName();
+}
+
+// useSpeakName();
+
+// CALLBACK FUNCTION
+
+function intervalRandom(min = 1000, max = 5000) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function f1(callback) {
+  setTimeout(() => {
+    console.log('f1');
+    if (callback) callback();
+  }, intervalRandom());
+}
+
+function f2(callback) {
+  setTimeout(() => {
+    console.log('f2');
+    if (callback) callback();
+  }, intervalRandom());
+}
+
+function f3(callback) {
+  setTimeout(() => {
+    console.log('f3');
+    if (callback) callback();
+  }, intervalRandom());
+}
+
+// f1(f1Callback);
+
+function f1Callback() {
+  f2(f2Callback);
+}
+
+function f2Callback() {
+  f3(f3Callback);
+}
+
+function f3Callback() {
+  console.log('Olá mundo!');
+}
+
+// console.log('Primeiro a ser executado!');
+
+// ===== FUNÇÕES IMEDIATAS | IIFE => IMMEDIATELY INVOKED FUNCTION EXPRESSION =====
+(() => {
+  console.log('Será executado na hora!');
+  console.log('Foge do escopo mais abrangente!');
+})();
